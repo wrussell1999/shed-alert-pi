f850ee13c77570fea6747e4425378094e9792d8e
William Russell <jetpowerss@gmail.com>
2019-07-14 09:36:37 +0100

    OpenCV works with default camera
    OpenCV displays the first camera on device. Works with SurfaceGo (on both cameras). Next is to test it on the Raspberry Pi

--- security_camera/camera.py
+++ security_camera/camera.py
@@ -1,2 +1,13 @@
+import cv2
+
 def main():
-    pass
+    camera = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = camera.read()
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) % 0xFF == ord('q'):
+            break
+    
+    camera.release()
+    cv2.destroyAllWindows()
